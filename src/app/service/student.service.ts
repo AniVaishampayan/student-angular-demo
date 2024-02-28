3a83a04297edd9cee477d0fad16eb9c53b519196
AniVaishampayan <aniket.vaishampayan@avisys.in>
2024-02-28 19:05:44 +0530

    Delete again files

--- src/app/service/student.service.ts
+++ src/app/service/student.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Student } from '../model/student.model';
 
 @Injectable()
 export class StudentService {
